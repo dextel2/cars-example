3b8816ad152277523f8d7ff5e50cb4115616d887
colshrapnel <col.shrapnel@gmail.com>
2016-06-30 14:47:03 +0300

    Update common.php
    Two essential things about connection:
    - you should always set a charset
    - you shoud always set errmode
    And two other measures that are not essential but improve the user experience:
    - set default fetch mode
    - set native mode for the prepared statements

--- common.php
+++ common.php
@@ -1,9 +1,16 @@
 <?php
 
+$db_host = "localhost";
 $db_name = "cars";
 $db_username = "root";
 $db_password = "";
+$db_charset = "utf8"
+$db_options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
         
 // Create a database connection handle $dbh
-
-$dbh = new PDO('mysql:host=localhost;dbname='. $db_name, $db_username, $db_password);
+$dsn = "mysql:host=$db_host;dbname=$db_name;charset=$db_charset";
+$dbh = new PDO($dsn, $db_username, $db_password, $db_options);
